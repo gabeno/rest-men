1e82fc7e9dab6e3f928b3a980c28d9760bf4b25e
Gabriel Majivu <gmajivu@gmail.com>
2023-04-19 14:21:49 +0300

    Add mongodb connection
    Use `.env` file to store connection details.

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import bodyParser from "body-parser";
 import compression from "compression";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import * as dotenv from "dotenv";
 import express from "express";
 import http from "http";
+import mongoose from "mongoose";
 
 const app = express();
+dotenv.config();
 
 app.use(cors({
   credentials: true,
@@ -18,4 +21,10 @@ const server = http.createServer(app);
 
 server.listen(8080, () => {
   console.log("Server running on http://localhost:8080/");
+});
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGO_URL);
+mongoose.connection.on("error", (err: Error) => {
+  console.log(`Error: ${err}`);
 });
