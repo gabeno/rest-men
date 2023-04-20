0ad3dc4e6082bc7c366c76f04aabb9a5c3f0829f
Gabriel Majivu <gmajivu@gmail.com>
2023-04-20 23:02:23 +0300

    Add update user controller and route

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUserById, getUsers } from "../db/users";
+import { deleteUserById, getUserById, getUsers } from "../db/users";
 
 export const deleteUser = async (req: express.Request, res: express.Response) => {
   try {
@@ -18,6 +18,28 @@ export const getAllUsers = async (req: express.Request, res: express.Response) =
     const users = await getUsers();
 
     res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const updateUser =async (req: express.Request, res: express.Response) => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+    if (!username) {
+      res.sendStatus(400);
+    }
+    
+    const user = await getUserById(id);
+    if (!user) {
+      return res.sendStatus(400);
+    }
+    user.username = username;
+    await user.save();
+
+    return res.status(201).json(user).end();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
