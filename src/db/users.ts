d4805eba58b0fc8d620cf19f7352e8ea9405f2e7
Gabriel Majivu <gmajivu@gmail.com>
2023-04-22 11:47:53 +0300

    Add createdAt and UpdatedAt timestamps ad make username unique

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   email: { type: String, required: true },
   authentication: {
     password: { type: String, required: true, select: false },
     salt: { type: String, select: false },
     sessionToken: { type: String, select: false },
   },
+}, {
+  timestamps: true
 });
 
 export const UserModel = mongoose.model("User", UserSchema);
